6a07e0a1d3374c2ce301ea561c37aaf710450473
ehoversten <ehoversten@gmail.com>
2021-09-08 20:34:51 -0400

    added logger to backend server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,22 @@
 require('dotenv').config();
 const express = require('express');
+const path = require("path");
+const logger = require('morgan');
 const PORT = process.env.PORT || 5001;
 
 const app = express();
+
+/* === Middleware === */
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+/* === Routing === */
+app.get('/', (req, res) => {
+    res.send("Testing Route");
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server is in ${process.env.NODE_ENV} mode on port: ${PORT}`);
