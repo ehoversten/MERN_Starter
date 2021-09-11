a03b0193a05427159d26007cbe33c56e5938ade1
ehoversten <ehoversten@gmail.com>
2021-09-11 17:52:34 -0400

    added initial User schema

---
+++ server/models/userModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
