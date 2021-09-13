cc8fd9213ac47d8638898a6c48e0297291a28103
ehoversten <ehoversten@gmail.com>
2021-09-13 19:12:27 -0400

    adding input validation to /register route

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -17,32 +17,80 @@ router.get('/all', async (req, res) => {
 router.post('/register', async (req, res) => {
     console.log(req.body);
 
-    let newUser = {
-        firstName: req.body.first,
-        lastName: req.body.last,
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
+    try {
+        const { first, last, username, email, password, confirm } = req.body;
+
+        // -- Input Validation -- //
+        if(!first || !last || !username || !email || !password || !confirm) {
+            return res
+                    .status(400)
+                    .json({ errorMessage: "Please Enter All Fields!"});
+        }
+
+        if(password.length > 6) {
+            return res  
+                    .status(400)
+                    .json({ errorMessage: "Password must be at least 6 characters!"});
+        }
+
+        if(password !== confirm) {
+            return res  
+                    .status(400)
+                    .json({ errorMessage: "Passwords must match!"});
+        }
+
+        let existingUser = User.findOne({ email });
+        console.log(existingUser);
+        if(existingUser) {
+            res.status(400)
+                .json({ errorMessage: "Email Already Exists"})
+        }
+
+        // -- Encrypt Password before Saving to Database -- //
+        let hashPass = password;
+
+        let newUser = {
+            firstName: first,
+            lastName: last,
+            username: username,
+            email: email,
+            password: hashPass
+        }
+
+
+    } catch(err) {
+        console.error(err);
+        res.status(500).send("ERROR ...");
     }
+
 
     console.log(newUser);
 
     // Create User in DB
-    try {
-        await User.create(newUser);
-        console.log("new user created ...")
-        res.json(newUser);
-    } catch(err) {
-        console.error(err);
-    }
+    // try {
+    //     await User.create(newUser);
+    //     console.log("new user created ...")
+    //     res.json(newUser);
+    // } catch(err) {
+    //     console.error(err);
+    // }
+
+    res.json(newUser);
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
     console.log(req.body);
 
     let loginObj = {
         email: req.body.email,
         password: req.body.password
+    }
+
+    try {
+        let found = await User.findOne();
+        console.log(found);
+    } catch(err) {
+        console.error(err);
     }
 
     res.json(loginObj);
