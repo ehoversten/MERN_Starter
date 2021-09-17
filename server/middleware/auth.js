dc0c53b2861e51d6b7490bb2f07ed39466c4cc6c
ehoversten <ehoversten@gmail.com>
2021-09-17 13:19:34 -0400

    added cookie-parser and auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+function auth(req, res, next) {
+    try {
+        const token = req.cookies.token;
+        console.log(token);
+
+        if(!token) res.status(401).json({ errorMessage: "Unauthorized" });
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+        // -- Testing -- // 
+        console.log(verified);
+
+        res.user = verified.user;
+        next();
+    } catch(err) {
+        console.error(err);
+        res.status(401).json({ errorMessage: "Unauthorized" });
+    } 
+}
+
+modules.export = auth;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require("path");
 const logger = require('morgan');
 const PORT = process.env.PORT || 3001;
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const routes = require('./routes');
@@ -14,7 +15,8 @@ const app = express();
 
 /* === Middleware === */
 app.use(logger('dev'));
-app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
